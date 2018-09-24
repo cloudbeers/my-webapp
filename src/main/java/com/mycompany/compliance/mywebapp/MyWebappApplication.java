4602353bdadd207da5b03957cd9d7d75b9eb47d1
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-09-24 17:12:52 +0200

    initial commit

---
+++ src/main/java/com/mycompany/compliance/mywebapp/MyWebappApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.compliance.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebappApplication.class, args);
+	}
+}
