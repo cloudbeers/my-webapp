738e02d7cb54387a7c758375d0dc6dabad4ad26c
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2019-06-01 15:21:35 +0200

    add a FIXME message

--- src/main/java/com/mycompany/governance/mywebapp/MyWebappApplicationController.java
+++ src/main/java/com/mycompany/governance/mywebapp/MyWebappApplicationController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyWebappApplicationController {
 
+	/**
+	 * FIXME something to do for PRJ-2
+	 * @return
+	 */
 	@GetMapping("/")
 	public String home(){
 		return "Hello governed world";
